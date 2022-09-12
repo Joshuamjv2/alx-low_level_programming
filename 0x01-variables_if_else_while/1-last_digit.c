c6ef16db2d4f5196dc89177d36a0f067ad19474c
Joshuamjv2 <joshuamjv22@gmail.com>
2022-09-12 05:04:35 -0700

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -33,7 +33,7 @@ int main(void)
 	}
 	else if (n % 10 < 6 && n % 10 != 0)
 	{
-		printf("%s %d is %d and less than 6 and not 0\n", str, n, n % 10);
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, n % 10);
 	}
 	else
 	{
