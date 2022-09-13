4c1a024e81d2a6377fa9f6332c64e42a40c18ff1
Joshuamjv2 <joshuamjv22@gmail.com>
2022-09-12 17:26:58 -0700

    number combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Combinations
+ *
+ * Description: Print all combinations of single digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
